dd8773c98fa7be7f8156adddf573b65d9c3afcdf
Norbert Volf <norbert@volf.eu>
2010-10-25 01:48:19 +0200

    add pgbuilder to create classes from pg table definiitions

---
+++ bin/pgbuilder.pl
@@ -0,0 +1,243 @@
+#!/usr/bin/perl
+#
+# Bere ze standrdniho vstupu definici tabulky z psql ziskanou pomoci prikazu \d nazev_tabulky
+#
+my $counter = 0;
+my $position = 0;
+my $rownumber = 0;
+my $primarykey;
+my $schema;
+my $table;
+my $title;
+my $definition = {
+	name => '',
+	messages => [],
+	form => {
+		url => "?type=json",
+		method_get => "",
+		method_set => "",
+		method_del => "",
+		method_del_caption => "Do you want delete",
+		method_del_url => "?method=group_search",
+		caption_edit => "Edit",
+		caption_save => "Save",
+		caption_cancel => "Cancel",
+		caption_delete => "Delete",
+	},
+	columns => [
+	],
+	autoloaders => {
+		test => {
+			class => 'Test::Item',
+			id => 'iditem',
+		},
+	},
+};
+
+while (<STDIN>) { # like "while(defined($_ = <STDIN>)) {"
+	chomp; # like "chomp($_)"
+	my $row = $_;
+
+	if ( $row =~ /(Tabulka|Table)\s"([^.]+)\.([^"]+).*/ ) {
+		$schema = $2;
+		$table = $3;
+		$title = join(" ",  map(ucfirst, split(/_/, $table)));
+		$definition->{name} = "$schema.$table";
+		$definition->{form}->{method_del_caption} = $definition->{form}->{method_del_caption} . " " . $title . "?";
+		$rownumber++;
+	} elsif ( $row =~ /\s*-+\+/ ) {
+		$rownumber++;
+	} elsif ( $row =~ /\s*(\w+)\s+\|\s([^|]+)\|(.*)/ && $rownumber < 2) {
+		$rownumber++;
+	} elsif ( ($row =~ /^\s*(\w+)\s+\|\s([^|]+)\|(.*)/ | $row =~ /^\s*(\w+)\s+\|\s([^|]+)\|$/) && $rownumber > 2) {
+		$counter++;
+		my $column = $1;
+		my $type = $2;
+		my $null = $3;
+		my $def = { name => $column };
+
+		push(@{$definition->{columns}}, $def);
+
+		if ( $type =~ /integer/ )  {
+			$def->{type} = "Cafe::Class::DB_INT";
+		} elsif ( $type =~ /character/ ) {
+			$def->{type} = "Cafe::Class::DB_VARCHAR";
+			if ( $type =~ /(\d+)/ ) {
+				$def->{opts} = "$1";
+			}
+		} elsif ( $type =~ /timestamp|date/ ) {
+			$def->{type} = "Cafe::Class::DB_DATE";
+		} elsif ( $type =~ /numeric|float/ ) {
+			$def->{type} = "Cafe::Class::DB_NUMERIC";
+		}
+
+		if ( $null =~ /not null/ )  {
+			$def->{null} = "Cafe::Class::DB_NOTNULL";
+		} else {
+			$def->{null} = "Cafe::Class::DB_NULL";
+		}
+
+
+		if ( $column !~ /^state$|^statestamp$|^stateuser$/ ) {
+			push(@{$definition->{messages}}, { name => $counter, value => $table . "_msgid_" . $counter });
+			$def->{msgid} = $counter;
+			$def->{rule} = "1";
+			$position++;
+			#Prvni polozku (obvykle PK) jen zobrazit
+			$def->{position} = $position;
+			if ( $counter > 1 ) {
+				$def->{label} = join(" ",  map(ucfirst, split(/_/, $column)));
+				$def->{input} = "text";
+				$def->{style} = {};
+				if ( $type =~ /integer/ )  {
+					$def->{style}->{input} = 'width:4em;';
+				} elsif ( $type =~ /character/ ) {
+					$def->{style}->{input} = "width:$def->{opts}em;";
+				} elsif ( $type =~ /timestamp|date/ ) {
+					$def->{style}->{input} = 'width:8em;';
+				} elsif ( $type =~ /numeric|float/ ) {
+					$def->{style}->{input} = 'width:6em;';
+				}
+			} else {
+				$def->{label} = 'Identifier';
+			}
+		}
+
+		#Predpokladam ze prvni polozka je primarni klic
+		if ( $counter == 1 ) {
+			$def->{primary_key} = "1";
+			$def->{sequence} = "$schema.$column";
+			$def->{null} = "Cafe::Class::DB_NULL";
+			$primarykey = $column;
+		}
+
+	}
+}
+
+
+#Generate translation keys table
+my $keys = "=pod\n";
+$keys .= ucfirst($title) . "|\n";
+$keys .= "$definition->{form}->{method_del_caption}|\n";
+foreach my $level1 (@{$definition->{columns}}) {
+	if ( exists ($level1->{label}) ) {
+		$keys .= ucfirst($level1->{label}) . "|\n";
+	}
+}
+$keys .= "=cut\n";
+
+
+
+#Generate name of persistent table
+my $output = "{\n";
+$output .= "\t\t\ttitle => '" . ucfirst($title) . "',\n";
+$output .= "\t\t\tname => '$definition->{name}',\n";
+
+#Generate messages of persistent table
+$output .= "\t\t\tmessages => {\n";
+foreach my $level1 (@{$definition->{messages}}) {
+	$output .= "\t\t\t\t$level1->{name} => undef,\n";
+}
+$output .= "\t\t\t},\n";
+
+#Generate forms
+$output .= "\t\t\tform => {\n";
+$output .= "\t\t\t\turl => '?type=json',\n";
+$output .= "\t\t\t\tmethod_get => '" . $table . "_get',\n";
+$output .= "\t\t\t\tmethod_set => '" . $table . "_set',\n";
+$output .= "\t\t\t\tmethod_del => '" . $table . "_del',\n";
+$output .= "\t\t\t\tmethod_del_caption => '$definition->{form}->{method_del_caption}',\n";
+$output .= "\t\t\t\tmethod_del_url => '?method=" . $table . "_search',\n";
+$output .= "\t\t\t\tcaption_edit => 'Edit',\n";
+$output .= "\t\t\t\tcaption_save => 'Save',\n";
+$output .= "\t\t\t\tcaption_cancel => 'Cancel',\n";
+$output .= "\t\t\t\tcaption_delete => 'Delete',\n";
+$output .= "\t\t\t},\n";
+
+#Generate columns of persistent table
+$output .= "\t\t\tcolumns => {\n";
+foreach my $level1 (@{$definition->{columns}}) {
+	$output .= "\t\t\t\t$level1->{name} => {\n";
+	$output .= "\t\t\t\t\ttype => $level1->{type},\n";
+	$output .= "\t\t\t\t\tnull => $level1->{null},\n";
+	if ( exists ($level1->{rule}) ) {
+		$output .= "\t\t\t\t\trule => $level1->{rule},\n";
+	}
+	if ( exists ($level1->{opts}) ) {
+		$output .= "\t\t\t\t\topts => $level1->{opts},\n";
+	}
+	if ( exists ($level1->{primary_key}) ) {
+		$output .= "\t\t\t\t\tprimary_key => $level1->{primary_key},\n";
+	}
+	if ( exists ($level1->{sequence}) ) {
+		$output .= "\t\t\t\t\tsequence => '$level1->{sequence}',\n";
+	}
+	$output .= "\n";
+	if ( exists ($level1->{msgid}) ) {
+		$output .= "\t\t\t\t\tmsgid => $level1->{msgid},\n";
+	}
+	if ( exists ($level1->{label}) ) {
+		$output .= "\t\t\t\t\tlabel => '$level1->{label}',\n";
+	}
+	if ( exists ($level1->{input}) ) {
+		$output .= "\t\t\t\t\tinput => '$level1->{input}',\n";
+	}
+	if ( exists ($level1->{position}) ) {
+		$output .= "\t\t\t\t\tposition => $level1->{position},\n";
+	}
+	if ( exists ($level1->{style}) && exists ($level1->{style}->{input})  ) {
+		$output .= "\t\t\t\t\tstyle => {\n";
+		$output .= "\t\t\t\t\t\tinput => '$level1->{style}->{input}',\n";
+		$output .= "\t\t\t\t\t},\n";
+	}
+
+	$output .= "\t\t\t\t},\n";
+
+}
+$output .= "\t\t\t},\n";
+
+#Generate autoloader example
+$output .= "\t\t\tautoloaders => {\n";
+foreach my $level1 (sort(keys(%{$definition->{autoloaders}}))) {
+	$output .= "#\t\t\t\t$level1 => {\n";
+	$output .= "#\t\t\t\t\tclass => '$definition->{autoloaders}->{$level1}->{class}',\n";
+	$output .= "#\t\t\t\t\tid => '$definition->{autoloaders}->{$level1}->{id}',\n";
+	$output .= "#\t\t\t\t\tshadow => '$definition->{autoloaders}->{$level1}->{shadow}',\n";
+	$output .= "#\t\t\t\t},\n";
+}
+$output .= "\t\t\t},\n";
+$output .= "\t\t}";
+
+$output ="package " . ucfirst($schema) . "::" . join("::",  map { ucfirst($_) } split('_', $table)) . ";
+
+use utf8;
+use warnings;
+use strict;
+use base qw(Cafe::Class);
+
+# Translation keys
+$keys
+
+sub new {
+	my (\$self, \$root, \$parent, \$$primarykey) = \@_;
+	my (\$instance) = \$self->SUPER::new(
+		\$root, 
+		\$parent,
+		$output
+	); 
+	bless(\$instance);
+
+	if ( \$$primarykey ) {
+		\$instance->{$primarykey} = \$$primarykey;
+		\$instance->load();	
+	}
+	return \$instance
+}
+
+sub DESTROY {
+	my (\$self) = \@_;
+}
+
+1;
+";
+print("$output\n");
---
+++ lib/Cafe/Export.pm
@@ -0,0 +1,330 @@
+package Cafe::Export;
+use strict;
+use utf8;
+
+use DBI;
+use Sys::Syslog;
+use URI;
+
+sub new {
+# {{{
+	my ($self, $root, $parent, $definition) = @_;
+	my ($instance) = {}; 
+	bless($instance);
+
+	if ( $definition ) {
+		$instance->{definition} = $definition;
+	} else {
+		die "Definition not found";
+	}
+
+	if ( $instance->{definition}->{source} ) {
+		$instance->{definition}->{source}->{dbh} = DBI->connect(@{$instance->{definition}->{source}->{connection}}) or $root->error("$!");
+		if ( exists($instance->{definition}->{source}->{before}) ) {
+			foreach my $query (@{$instance->{definition}->{source}->{before}}) {
+				$instance->{definition}->{source}->{dbh}->do($query);
+			}
+		}
+		if ( !exists($instance->{definition}->{source}->{autocommit}) ){
+			$instance->{definition}->{source}->{autocommit} = 1;
+		}
+	}
+
+	if ( $instance->{definition}->{destination} ) {
+		$instance->{definition}->{destination}->{dbh} = DBI->connect(@{$instance->{definition}->{destination}->{connection}}) or $root->error("$!");
+		if ( exists($instance->{definition}->{destination}->{before}) ) {
+			foreach my $query (@{$instance->{definition}->{destination}->{before}}) {
+				$instance->{definition}->{destination}->{dbh}->do($query);
+			}
+		}
+		if ( !exists($instance->{definition}->{destination}->{autocommit}) ){
+			$instance->{definition}->{destination}->{autocommit} = 1;
+		}
+	}
+
+	#Save reference to instance of Apache::Script class
+	$instance->{root} = $root;
+
+	return $instance;
+# }}}
+}
+
+sub export {
+# {{{
+        my ($self) = @_;
+
+        foreach my $table (@{$self->{definition}->{tables}}) {
+		if ( ! $table->{disabled} ) {
+			$self->{root}->log("Exporting $table->{desc}\n");
+
+			if ( $self->{definition}->{destination} ) {
+				if ( ! $self->{definition}->{destination}->{autocommit} ) {
+					$self->{definition}->{destination}->{dbh}->begin_work();
+				}
+			}
+	
+			$self->export_query($table);
+
+			if ( $self->{definition}->{destination} ) {
+				if ( ! $self->{definition}->{destination}->{autocommit} ) {
+					$self->{definition}->{destination}->{dbh}->commit();
+				}
+			}
+		}
+        }
+# }}}
+}
+
+sub export_query {
+# {{{
+	my ( $self, $table ) = @_;
+	my ($sth, $last, $sth_update, $sth_delete, $sth_del_log);
+	my $cnt = 0;
+	my $sth_insert;
+	my @to_arr = ();
+	my @param_arr = ();
+
+	#Execute qyery for source data
+	$sth = $self->{definition}->{source}->{dbh}->prepare($table->{from}->{def});
+	$sth->execute() or die "$! (SQL: $table->{from}->{def})";
+
+	#Define queries for destination table if is defined
+	if ( exists($table->{to}) ) {
+		#Define insert query
+		foreach my $field (@{$table->{to}->{fields}}) {
+			push(@to_arr, $field);
+			push(@param_arr, "?");
+		}
+
+		my $sql = "INSERT INTO $table->{to}->{def} (" . join(",", @to_arr) . ") VALUES ("  . join(",", @param_arr) . ")";
+		$sth_insert = $self->{definition}->{destination}->{dbh}->prepare($sql);
+
+		#Define update query
+		if ( exists($table->{operation})) {
+			@to_arr = ();
+			foreach my $field (@{$table->{to}->{fields}}) { push(@to_arr, "$field = ?"); }
+			$sql = "UPDATE $table->{to}->{def} SET " . join(",", @to_arr) . " WHERE $table->{operation}->{column_destination_primary_key} = ?";
+			$sth_update = $self->{definition}->{destination}->{dbh}->prepare($sql);
+		}
+
+		#Define delete query
+		if ( exists($table->{operation})) {
+			$sql = "DELETE FROM $table->{to}->{def} WHERE $table->{operation}->{column_destination_primary_key} = ?";
+			$sth_delete = $self->{definition}->{destination}->{dbh}->prepare($sql);
+		}
+	}
+
+	if ( $self->{definition}->{destination}->{autocommit} )	{ 
+		$self->{definition}->{destination}->{dbh}->begin_work();
+	};
+	if ( $self->{definition}->{source}->{autocommit} )	{ 
+		$self->{definition}->{source}->{dbh}->begin_work();
+	};
+
+	#Truncate full copy tables
+	if ( 	! $table->{from}->{use_last_key} 
+		&& exists($table->{to})
+		&& ! exists($table->{operation})
+		&& ! $table->{notruncate}
+	) {
+		$self->{definition}->{destination}->{dbh}->do("TRUNCATE $table->{to}->{def}");
+	}
+
+	#Run before event
+	if ( $table->{before} ) {
+		&{$table->{before}}($self);
+	}
+
+	#Copy data to destination table
+	while ( my $row = $sth->fetchrow_hashref() ) {
+		if ( $table->{listing} && $table->{listing} > 0 ) {
+			if ( ++$cnt  % $table->{listing} == 0 ) {
+				$self->{root}->log("$cnt rows replicated " . ( exists($table->{to}) ? "to $table->{to}->{def}" : "by $table->{desc}" ) . ".\n");
+			}
+		}
+
+		@param_arr = ();
+		
+		if ( $table->{from}->{event} ) {
+			&{$table->{from}->{event}}($self, $row);
+		}
+
+		foreach my $field (@{$table->{from}->{fields}}) {
+			if ( ref($field) eq "HASH") { 
+				my $text;
+				if ( $field->{event} ) {
+					$text = &{$field->{event}}($self, $row->{$field->{column}});
+				}
+				push(@param_arr, $text);
+			} elsif ($field) { 
+				push(@param_arr, $row->{$field});
+			} else {
+				push(@param_arr, undef);
+			}
+		}
+
+		if ( exists($table->{operation}) && exists($table->{to}) ) {
+			if ( ( $row->{$table->{operation}->{column_source_operation}} & 1 ) == 1 ) {
+				$sth_insert->execute(@param_arr) or die "$!";
+			}
+			if ( ( $row->{$table->{operation}->{column_source_operation}} & 2 ) == 2 ) {
+				push(@param_arr, $row->{$table->{operation}->{column_source_primary_key}});
+				$sth_update->execute(@param_arr) or die "$!";
+			}
+			if ( ( $row->{$table->{operation}->{column_source_operation}} & 4 ) == 4 ) {
+				$sth_delete->execute($row->{$table->{operation}->{column_source_primary_key}}) or die "$!";
+			}
+			$self->{definition}->{source}->{dbh}->do("
+				DELETE FROM $table->{operation}->{table_log} 
+					WHERE $table->{operation}->{column_log_id} = $row->{$table->{operation}->{column_source_primary_key}}
+						AND $table->{operation}->{column_log_name} = '$table->{operation}->{table_source}'
+			")  or die "$!";
+		} elsif( exists($table->{to}) )  {
+			$sth_insert->execute(@param_arr) or die "$!";
+		}
+
+		#Remember last primary key value
+		if ( $table->{from}->{use_last_key} ) {
+			$last->{new_value} = $row->{$last->{column_name}};
+		}
+	}
+	#Save last key value to definition table
+	if ( $table->{from}->{use_last_key} && exists($last->{new_value}) ) {
+		$self->{root}->log("Save new last value for for key $last->{column_name} and value $last->{new_value}.\n");
+		$self->last($table->{from}->{use_last_key}, $last->{new_value});
+	}
+	
+	#Run after event
+	if ( $table->{after} ) {
+		&{$table->{after}}($self);
+	}
+
+	if ( $table->{listing} && $table->{listing} > 0 ) {
+		$self->{root}->log("Done $table->{desc} with $cnt rows.\n");
+	}
+
+	if ( $self->{definition}->{destination}->{autocommit} ){
+		$self->{definition}->{destination}->{dbh}->commit();
+	};
+	if ( $self->{definition}->{source}->{autocommit} ){
+		$self->{definition}->{source}->{dbh}->commit();
+	};
+# }}}
+}
+
+sub last {
+# {{{
+	my ( $self, $idlast_key_value, $newvalue ) = @_;
+	my $retval;
+
+	if (  defined( $newvalue ) ) {
+		$retval = $newvalue;
+		$self->{root}->{dbh}->do("UPDATE schema.last_key_values SET last_value = ? WHERE idlast_key_value = ?", undef, $newvalue, $idlast_key_value) or 
+			die "Error with save newlast value to database: @!";
+	}
+
+	my $sth = $self->{root}->{dbh}->prepare(q(
+		SELECT  
+			last_value,
+			idlast_key_value,
+			table_name,
+			column_name
+			FROM schema.last_key_values 
+			WHERE idlast_key_value = ?
+	));
+	$sth->execute($idlast_key_value);
+	if ( my $row = $sth->fetchrow_hashref() ) {
+		$retval = $row;
+	}
+	$sth->finish();
+
+	if ( ! defined($retval) ) { die "Error with get last_key_value" } 
+
+	return($retval);
+# }}}
+}
+
+sub DESTROY {
+	my ($self) = @_;
+}
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Cafe::Export - module is used for script inside My Framework
+
+=head1 SYNOPSIS
+
+use Cafe::Export;
+
+my $definition = {
+	source => {
+		connection => ['dbi:Pg:dbname=databaze;host=mujhost', 'uzivatel', 'heslo'],
+		before => [
+			q(SET client_encoding = 'UTF-8')
+		],
+	},
+	destination => {
+		connection => ['dbi:mysql:database=databaze', 'uzivatel', 'heslo'],
+	},
+	tables => [
+		{ 	
+			desc	=> 'Import of st_prijem',
+			disabled => 0,
+			from    => {
+				def => q(
+					SELECT 
+						idprijem,
+						value,
+						amount,
+						desc
+						FROM st_prijem
+				),
+				event => sub {
+					my ($self, $row) = @_;
+					$row->{idprijem} = $row->{idprijem} * 100;
+				}
+				fields => [
+					'idprijem',
+					{
+						name => 'value',
+						event => sub {
+							my ($self, $text) = @_;
+							$text = "new_value";
+							return($text);
+						}
+					},
+					'amount',
+					'desc'
+				]
+			},
+			to => {
+				def => 'mirror.st_prijem',
+				fields => [
+					'idprijem',
+					'value',
+					'amount.,
+					'desc'
+				]
+			}
+		},
+	]
+};
+
+my $script = new Apache::Script("Iris::Application", "/var/www/perl/", "dbi:Pg:dbname=bata;host=klinger.hcbr.bata.cz", 'robot', 'Laexee0e');
+my $export = new Apache::Export($script, $script, $definition);
+$export->export();
+
+=head1 FUNCTIONS
+
+=over 8
+
+=item last KEY, NEWVALUE
+
+return last value from framework database, if NEWVALUE is defined method set new value
+
+=back
