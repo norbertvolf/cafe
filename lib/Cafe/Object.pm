914c1015dd1b09f5832d5bdb65e7907a9f9a2c84
Norbert Volf <norbert@volf.eu>
2011-01-18 10:54:15 +0100

    fix stack trace printing

--- lib/Cafe/Object.pm
+++ lib/Cafe/Object.pm
@@ -26,7 +26,6 @@ sub die {
 }
 #}}}
 
-
 #{{{ print_stack
 sub print_stack {
 	my $self = shift;
@@ -36,7 +35,7 @@ sub print_stack {
 	while ( (my @call_details = (caller($i++))) && ($i<$max_depth) ) {
 		print(STDERR "$call_details[1] line $call_details[2] in function $call_details[3]\n");
 	}
-	print("--- End stack trace ---\n\n");
+	print(STDERR "--- End stack trace ---\n\n");
 }
 #}}}
 1;
