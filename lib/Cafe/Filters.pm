1ba580588a786c60423c56aeafeedf7efa03052a
Norbert Volf <norbert@volf.eu>
2011-04-17 22:47:55 +0200

    Add a2ps filter

--- lib/Cafe/Filters.pm
+++ lib/Cafe/Filters.pm
@@ -13,12 +13,11 @@ use POSIX;
 #-------------------------------------------
 #Template toolkit filters
 
-=item Filter html_zipcode
+#{{{ tex_zipcode
+=head2 Filter html_zipcode
 Formating zipcode
 =cut
-
 sub  tex_zipcode {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -28,14 +27,13 @@ sub  tex_zipcode {
 		}
 		return($text);
 	}
-# }}}
 }
-
-=item Filter shoesize 
+#}}}
+#{{{ shoesize
+=head2 Filter shoesize 
 Formating shoesize for html
 =cut
 sub shoesize {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -47,11 +45,10 @@ sub shoesize {
 		}
 		return($text);
 	}
-# }}}
 }
-
+#}}}
+#{{{ article
 sub article {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -67,12 +64,10 @@ sub article {
 		}
 		return($text);
 	}
-# }}}
 }
-
-
+#}}}
+#{{{ location
 sub location {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -84,11 +79,10 @@ sub location {
 		}
 		return($text);
 	}
-# }}}
 }
-
+#}}}
+#{{{ phonenumber
 sub phonenumber {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -98,11 +92,10 @@ sub phonenumber {
 		};
 		return($text);
 	}
-# }}}
 }
-
+#}}}
+#{{{ iso2utf
 sub iso2utf {
-# {{{
 	my ($context)  = @_;
 
   return sub {
@@ -110,11 +103,10 @@ sub iso2utf {
     my $converter = Text::Iconv->new("ISO-8859-2", "UTF-8");
     return($converter->convert($text));
   }
-# }}}
 }
-
+#}}}
+#{{{ texclean
 sub texclean {
-# {{{
 	my ($context)  = @_;
 
 	return sub {
@@ -193,12 +185,11 @@ sub texclean {
 
 		return($text);
 	}
-# }}}
 }
-
+#}}}
+#{{{ cslatex_filter_factory
 # if cslatextwice ... it calls cslatex twice. It is necessary to call it twice when you need page references to be printed
 sub cslatex_filter_factory {
-# {{{
     my($context, $output, $options, $cslatextwice) = @_;
 	
     $output = lc($output);
@@ -344,16 +335,15 @@ sub cslatex_filter_factory {
         $context->throw("$fName not yet supported on $^O OS."
                       . "  Please contribute code!!");
     }
-# }}}
 }
-
+#}}}
+#{{{ sprintf
 =head2 Filter sprintf
 
 Filter provide sprintf funcionality as TT2 filter
 
 =cut
 sub sprintf {
-# {{{
     my ($context, $format, $use_thousands_sep)  = @_;
     return sub {
         my $number = shift;
@@ -389,16 +379,15 @@ sub sprintf {
 	}
         return($formatted);
     }
-# }}}
 }
-
+# }}}
+#{{{ utf8_email_header
 =head2 Filter utf8_email_header
 
 Convert text to base64
 
 =cut
 sub utf8_email_header {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -409,12 +398,10 @@ sub utf8_email_header {
 		$text = '=?UTF-8?B?'.$text.'?=';
 		return($text);
 	}
-# }}}
 }
-
-
+#}}}
+#{{{ hostaddress
 sub hostaddress {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -424,11 +411,10 @@ sub hostaddress {
 		}
 		return($text);
 	}
-# }}}
 }
-
+# }}}
+#{{{ utf2iso
 sub utf2iso {
-# {{{
 	my ($context)  = @_;
 
 	return sub {
@@ -436,16 +422,15 @@ sub utf2iso {
 		my $converter = Text::Iconv->new("UTF-8", "ISO-8859-2");
 		return($converter->convert($text));
 	}
-# }}}
 }
-
+# }}}
+#{{{ csvclean
 =head2  Filter csvclean
 
 Covnert char ";" to ","
 
 =cut
 sub csvclean {
-# {{{
 	my ($context)  = @_;
 
 	return sub{
@@ -455,24 +440,18 @@ sub csvclean {
 		$text =~ s/\r/ /g;
 		return($text);
 	}
-# }}}
 }
-=head2  Filter csvclean
-
-Covnert char ";" to ","
-
-=cut
-
+#}}}
+#{{{ default
 sub default {
-# {{{
 	my ($context, $defaultvalue)  = @_;
 	return sub {
 		my $value = shift;
 		return ($value) ? $value : $defaultvalue;
 	};
-# }}}
 }
-
+#}}}
+#{{{ perl_decode
 =head2  Filter perl_decode
 
 Returns value decoded to perl internal encoding (unicode) from $charset
@@ -480,104 +459,119 @@ Returns value decoded to perl internal encoding (unicode) from $charset
 =cut
 
 sub perl_decode {
-# {{{
 	my ($context, $charset)  = @_;
 	return sub {
 		my $value = shift;
 		my $charset = shift;
 		return Encode::decode($charset ? $charset : "utf-8", $value);
 	};
-# }}}
 }
-
+# }}}
+#{{{ perl_encode
 sub perl_encode {
-# {{{
 	my ($context, $charset)  = @_;
 	return sub {
 		my $value = shift;
 		my $charset = shift;
 		return Encode::encode($charset ? $charset : "utf-8", $value);
 	};
-# }}}
 }
-
+#}}}
+#{{{ remove_diacritic
 =head2 Filter remove_diacritic
 
 Remove diacritic from text
 
 =cut
-
 sub remove_diacritic {
-# {{{
 	my ($context) = @_;
 	return sub {
 		my $text = shift;
 		$text =~ tr/ŁĽŞŠŻłšşźžżŔÁÂĂÄĹĆÇČÉĘËĚÍÎĎĐŃŇÓÔŐÖŘŮÚŰÜÝŤŢŽŕáâăäĺćçčéęëěíîďđńňóôőöřůúűüýţť\x{fffd}/LLSSZlsszzzRAAAALCCCEEEEIIDDNNOOOORUUUUYTTZraaaalccceeeeiiddnnooooruuuuyttl/;
 		return($text);
 	};
-# }}}
 }
-
+#}}}
+#{{{ a2ps
 =head2  Filter a2ps
 
 Create pdf from text
-a2ps --center-title="dispatch note" --right-title="%Q" --left-title="" --header="" --right-footer="" --left-footer="" -1  -o /tmp/aa.ps *.txt
-=cut
 
+=cut
 sub a2ps {
-# {{{
-	my ($context, $withNewPages, $title, $charsPerLine) = @_;
+	my ($context, $options) = @_;
 	return sub {
 		my $text = shift;
-		my $tmpDir = tempdir("/tmp/dispatchnote-a2ps-XXXXX");
-		my $output = "";
+		my $tmpfile =  File::Spec->catfile(File::Spec->tmpdir(), "cafe-a2ps-$$.pdf");
+		my @params_a2ps;
+		my @params_ghost;
+		my $pdf = "";
 
-		my @fileNames = ();
-
-		if ($withNewPages){
-			my @pages = split (/<newpage\/>[\ \t\n]*/,$text);
-			my $count = 1;
-			foreach my $page (@pages){
-				my $filename = File::Spec->catfile($tmpDir, sprintf("page-%02d.txt",$count));
-				$count += 1;
-				
-				open (FH,"> $filename") or die "cannot open $filename: $!";
-				print FH $page;
-				close (FH);
-
-				push @fileNames, $filename;
+		#Prepare parameters
+		if ( ref($options) eq "HASH" ) {
+			push(@params_a2ps, "--landscape") if ( $options->{landscape} );
+			push(@params_a2ps, "--portrait") if ( $options->{portrait} );
+			push(@params_a2ps, "--no-header") if ( $options->{'no-header'} );
+			push(@params_a2ps, "--columns=$options->{columns}") if ( $options->{columns} );
+			if ( $options->{medium} ) {
+				push(@params_a2ps, "--medium=$options->{medium}");
+				push(@params_ghost, "-sPAPERSIZE=" . lc($options->{medium}) ); 
 			};
-		} else {
-			my $filename = File::Spec->catfile($tmpDir, "text.txt");
-			open (FH, "> $filename") or die "cannot open $filename: $!";
-			print FH $text;
-			close FH;
-			push @fileNames, $filename;
+			push(@params_a2ps, "--chars-per-line=" . $options->{'chars-per-line'}) if ( $options->{'chars-per-line'} );
+			push(@params_a2ps, "--lines-per-page=" . $options->{'lines-per-page'}) if ( $options->{'lines-per-page'} );
+			push(@params_a2ps, "--borders=$options->{borders}") if ( $options->{borders} );
+			push(@params_a2ps, "--footer='$options->{footer}'") if ( $options->{footer} );
+			push(@params_a2ps, "--header") if ( $options->{header} );
+			push(@params_a2ps, "--left-title='" . $options->{'left-title'} . "'") if ( $options->{'left-title'} );
+			push(@params_a2ps, "--right-title='" . $options->{'right-title'} . "'") if ( $options->{'right-title'} );
+			push(@params_a2ps, "--center-title='" . $options->{'center-title'} . "'") if ( $options->{'center-title'} );
+			push(@params_a2ps, "--left-footer='" . $options->{'left-footer'} . "'") if ( $options->{'left-footer'} );
+			push(@params_a2ps, "--right-footer='" . $options->{'right-footer'} . "'") if ( $options->{'right-footer'} );
+			push(@params_a2ps, "--title='" . $options->{'title'} . "'") if ( $options->{'title'} );
 		}
-		my $psfile = File::Spec->catfile($tmpDir,"result.ps");
-		my $pdffile = File::Spec->catfile($tmpDir,"result.pdf");
-		my $cmd = sprintf('a2ps --title="%s" --center-title="%s" --right-title="%Q" --left-title="" --borders=0 --no-header --right-footer="" --left-footer="" --chars-per-line=%d --columns=1 -o "%s" %s 2> /dev/null 1> /dev/null',
-				  $title,
-				  $title,
-				  $charsPerLine,
-				  $psfile,
-				  join(" ", @fileNames)
-		    );
-		if( system($cmd) == 0 ){
-			$cmd = sprintf("ps2pdf '%s' '%s'",$psfile,$pdffile);
-			if( system($cmd) == 0 ){
-				if ( open(FH, $pdffile) ) {
-					local $/ = undef;       # slurp file in one go
-					binmode(FH);
-					$output = <FH>;
-					close(FH);
-				};
-			};
-		};
-		rmtree($tmpDir);
-		return $output;
+
+		#Run text to pdf conversion
+		open(OUTPUT, "| a2ps " . join(" ", @params_a2ps) . " -o - 2> /dev/null | ps2pdf " . join(" ", @params_ghost) . " - - > $tmpfile" ) or die "Can't start a2ps conversion: $!";
+		print OUTPUT $text;
+		close(OUTPUT);
+
+		#Read generated PDF
+		open(PDF, $tmpfile ) or die "Can't open $tmpfile : $!";
+		while (<PDF>)  { $pdf .= $_; }
+		close(PDF);
+		unlink($tmpfile);
+		return $pdf;
+=pod
+		Not defined a2ps params
+		--rows=NUM number of rows per sheet
+		--major=DIRECTION first fill (DIRECTION=) rows, or columns -1, -2, ..., -9 predefined font sizes and layouts for 1.. 9 virtuals
+		-A, --file-align=MODE align separate files according to MODE (fill, rank page, sheet, or a number)
+		-j, --borders* print borders around columns
+		--margin[=NUM] define an interior margin of size NUM The options -1.. -9 affect several primitive parameters to set up predefined layouts with 80 columns. Therefore the order matters: '-R -f40 -2' is equivalent to '-2'. To modify the layout, use '-2Rf40', or compose primitive options ('--columns', '--font-size' etc.).  Virtual pages:
+		--line-numbers=NUM precede each NUM lines with its line number
+		-C alias for --line-numbers=5
+		-f, --font-size=SIZE use font SIZE (float) for the body text
+		-m, --catman process FILE as a man page (same as -L66)
+		-T, --tabsize=NUM set tabulator size to NUM
+		--non-printable-format=FMT specify how non-printable chars are printed
+
+		Headings:
+		-B, --no-header no page headers at all
+		-u, --underlay[=TEXT] print TEXT under every page
+
+		Input:
+		-a, --pages[=RANGE] select the pages to print
+		-c, --truncate-lines* cut long lines
+		-i, --interpret* interpret tab, bs and ff chars
+		--end-of-line=TYPE specify the eol char (TYPE: r, n, nr, rn, any)
+		-X, --encoding=NAME use input encoding NAME
+		--stdin=NAME set the name of the input file stdin
+		--print-anyway* force binary printing
+		-Z, --delegate* delegate files to another application
+		--toc[=TEXT] generate a table of content When delegations are enabled, a2ps may use other applications to handle the processing of files that should not be printed as raw information, e.g., HTML PostScript, PDF etc.
+=cut
 	};
-# }}}
 };
+# }}}
 
 1;
