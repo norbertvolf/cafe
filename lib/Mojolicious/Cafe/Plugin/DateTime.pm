fb7b62f35db85403aebee19c55ff6780c3a66fd6
Norbert Volf <volf@bata.cz>
2012-05-03 16:20:24 +0200

    add timezone to now method

--- lib/Mojolicious/Cafe/Plugin/DateTime.pm
+++ lib/Mojolicious/Cafe/Plugin/DateTime.pm
@@ -3,11 +3,15 @@ package Mojolicious::Cafe::Plugin::DateTime;
 use Mojo::Base 'Mojolicious::Plugin';
 use DateTime;
 
+#Return actuale time.
+#If timezone is defined, return timezone based time
+#If not use localtime from server
 sub register {
 	my ($self, $app) = @_;
 	$app->helper( 
 		cnow => sub {
-			return(DateTime->now);
+			my ($self, $timezone) = @_;
+			return( DateTime->from_epoch( epoch => time(), time_zone => ( $timezone // 'Europe/Prague') ) );
 		}
 
 	);
