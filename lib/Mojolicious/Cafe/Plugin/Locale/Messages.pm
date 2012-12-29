aeb1763783a65bb2cf1b7defc4eadc3159a0c66c
Norbert Volf <norbert@volf.eu>
2012-12-29 17:38:28 +0100

    remove controller dependency in messages plugin

--- lib/Mojolicious/Cafe/Plugin/Locale/Messages.pm
+++ lib/Mojolicious/Cafe/Plugin/Locale/Messages.pm
@@ -12,9 +12,7 @@ sub register {
 		__ => sub {
 			my $c = shift; 
 			my $msgid = shift;
-			$c->set_locale($c->user->locale);
 			my $retval = decode('utf-8', gettext($msgid));
-			$c->restore_locale($c->user->locale);
 			return($retval);
 		}
 	);
